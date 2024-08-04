f85a8bb10acde45f418abf10bf90ff9089fb2d67
SeieunYoo <isho48239@gmail.com>
2024-08-04 16:01:05 +0900

    feat: 스토리북 세팅

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,7 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@wow-class/eslint-config/basic.js"],
+  extends: [
+    "@wow-class/eslint-config/basic.js",
+    "plugin:storybook/recommended",
+    "plugin:storybook/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
--- packages/ui/src/Button/index.tsx
+++ packages/ui/src/Button/index.tsx
@@ -2,7 +2,7 @@
 
 import type { ReactNode } from "react";
 
-interface ButtonProps {
+export interface ButtonProps {
   children: ReactNode;
   className?: string;
   appName: string;
