fde5847648037945d0816a3a8ed98f3d39ed2f35
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-08 18:05:03 +0900

    chore: 불필요한 내용 삭제

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,11 +1,9 @@
 import { styled } from "@styled-system/jsx";
-import { Button } from "@wow-class/ui";
 
 const Home = () => {
   return (
     <div>
       Home
-      <Button appName="admin">버튼</Button>
       <styled.div color="blue.100">sdf</styled.div>
       <styled.div color="yellow.500">sdf</styled.div>
     </div>
