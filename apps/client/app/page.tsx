b6d376c48c4d51ab2010db606bd72c04d9230307
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-09 13:31:44 +0900

    feat: 클라이언트 페이지 레이아웃 구축

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,13 +1,5 @@
-import { styled } from "@styled-system/jsx";
-
 const Home = () => {
-  return (
-    <div>
-      Home
-      <styled.div color="blue.100">sdf</styled.div>
-      <styled.div color="yellow.500">sdf</styled.div>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
