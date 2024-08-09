178f09600e198873c813b4b7f91821d727ab6fd0
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-09 20:22:30 +0900

    feat: admin home으로 접속 시 studies 페이지로 리다이렉트되도록 설정

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/studies",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
