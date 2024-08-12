549dd71fa91fa124dc866991ee77f5299cbe84dd
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-12 23:15:11 +0900

    feat: landing 페이지 레이아웃 구현

---
+++ apps/client/app/(beforeLogin)/landing/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { css } from "@styled-system/css";
+import { Header } from "@wow-class/ui/client";
+import { useRouter } from "next/navigation";
+import Button from "wowds-ui/Button";
+
+const LandingPage = () => {
+  const router = useRouter();
+
+  const handleClickLogin = () => {
+    router.push("/auth");
+  };
+
+  return (
+    <div className={containerStyle}>
+      <Header />
+      <main className={mainContentStyle}>
+        <div className={taglineStyle}>
+          와우클래스와 함께 <br />
+          GDSC에서 스터디해요!
+        </div>
+        <div className={descriptionStyle}>
+          GDSC Hongik은 홍익대학교의 학생 개발자들을 위해 <br />
+          개발에 쉽게 입문할 수 있는 기회를 제공해요.
+        </div>
+        <Button size="lg" onClick={handleClickLogin}>
+          로그인
+        </Button>
+      </main>
+    </div>
+  );
+};
+
+export default LandingPage;
+
+const containerStyle = css({
+  height: "100vh",
+  display: "flex",
+  flexDirection: "column",
+});
+
+const mainContentStyle = css({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+  height: "100%",
+});
+
+const taglineStyle = css({
+  color: "#17191C",
+  fontSize: "74px",
+  fontWeight: 800,
+  lineHeight: "145%",
+  letterSpacing: "-0.74px",
+  marginBottom: "40px",
+});
+
+const descriptionStyle = css({
+  color: "sub",
+  textStyle: "body0",
+  marginBottom: "48px",
+});
