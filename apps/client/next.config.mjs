54bd680347d4cad4c273b82aaa78381521f7edf9
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-12 21:41:08 +0900

    chore: client 접속 시 auth 페이지로 접속하도록 수정

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/my-study",
+        destination: "/auth",
         permanent: false,
         statusCode: 301,
       },
