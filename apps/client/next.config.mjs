3fca134ddfa94406b3523b831a64cd3ce16f24c0
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-13 00:07:39 +0900

    chore: 접속 시 landing 페이지로 리다이렉트되도록 수정

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/auth",
+        destination: "/landing",
         permanent: false,
         statusCode: 301,
       },
