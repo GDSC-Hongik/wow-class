cf7869a00d5a84928bf56ca5cf25ea277fa2c3c8
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-13 22:57:20 +0900

    refactor: dashboard api 별도 함수로 분리

--- apps/client/app/(beforeLogin)/social-login/redirect/page.tsx
+++ apps/client/app/(beforeLogin)/social-login/redirect/page.tsx
@@ -1,29 +1,20 @@
 import { fetcher } from "@wow-class/utils";
-import { apiPath } from "constants/apiPath";
+import { dashboardApi } from "apis/dashboardApi";
 import { routePath } from "constants/routePath";
-import { tags } from "constants/tags";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import type { DashboardApiResponseDto } from "types/dtos/auth";
 
 const AuthServerRedirectPage = async () => {
   const cookieStore = cookies();
   const accessToken = cookieStore.get("accessToken")?.value;
 
-  const response = await fetcher.get<DashboardApiResponseDto>(
-    apiPath.dashboard,
-    {
-      next: { tags: [tags.dashboard] },
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-    }
-  );
+  fetcher.setDefaultHeaders({
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${accessToken}`,
+  });
 
-  const memberRole = response.data?.member.role;
-  const currentRecruitmentOpen =
-    response.data?.currentRecruitmentRound.period.open;
+  const { memberRole, currentRecruitmentOpen } =
+    await dashboardApi.getDashboardInfo();
 
   if (memberRole === "REGULAR") {
     redirect(routePath["my-study"]);
