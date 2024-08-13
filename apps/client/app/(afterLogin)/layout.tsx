077cb95b0c6b4155a57369ddd0c63424045a3940
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-13 15:28:11 +0900

    feat: 로그인 상태 관련 리다이렉트 처리 추가

--- apps/client/app/(afterLogin)/layout.tsx
+++ apps/client/app/(afterLogin)/layout.tsx
@@ -1,6 +1,15 @@
 import Navbar from "components/Navbar";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const cookieStore = cookies();
+  const isLogin = !!cookieStore.get("accessToken");
+
+  if (!isLogin) {
+    redirect("/auth");
+  }
+
   return (
     <>
       <Navbar />
