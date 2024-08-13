9c355b27e1722c7ead73631dd1ccdbda7251e227
SeieunYoo <isho48239@gmail.com>
2024-08-13 16:46:23 +0900

    feat: 텍스트 컴포넌트 as 추가, typo 관련 panda 설정 panda-config 로 이동

--- packages/ui/panda.config.ts
+++ packages/ui/panda.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from "@pandacss/dev";
 import commonConfig from "@wow-class/panda-config/common-config";
 
-import { typography } from "wowds-tokens";
-
 export default defineConfig({
   ...commonConfig,
-  staticCss: {
-    css: [
-      {
-        properties: {
-          textStyle: Object.keys(typography),
-        },
-      },
-    ],
-  },
   dependencies: ["./src/components/**"],
   include: ["./src/**/*.{ts,tsx,js,jsx}"],
 });
---
+++ packages/ui/src/components/Text/Text.stories.tsx
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Text from ".";
+
+const meta: Meta<typeof Text> = {
+  title: "UI/Text",
+  component: Text,
+  tags: ["autodocs"],
+  parameters: {
+    componentSubtitle: "Text 컴포넌트",
+  },
+  argTypes: {
+    as: {
+      control: { type: "select" },
+      options: ["p", "h1", "h2", "h3", "span", "div"],
+      description: "HTML 태그 이름",
+    },
+    typo: {
+      control: { type: "text" },
+      description: "텍스트 타이포",
+    },
+    color: {
+      control: { type: "color" },
+      description: "텍스트 색상",
+    },
+    children: {
+      control: { type: "text" },
+      description: "텍스트 내용",
+    },
+    style: {
+      control: { type: "object" },
+      description: "커스텀 스타일 객체",
+    },
+    className: {
+      control: { type: "text" },
+      description: "커스텀 클래스 이름",
+    },
+  },
+} satisfies Meta<typeof Text>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: "예시",
+    as: "p",
+    typo: "body1",
+    color: "textBlack",
+  },
+};
+
+export const AsDiv: Story = {
+  args: {
+    children: "예시",
+    as: "div",
+    typo: "h1",
+    color: "primary",
+  },
+};
