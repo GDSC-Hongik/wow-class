6c4a8e186cfba77636bb4b42ea09ff8c903e02ed
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-13 23:18:19 +0900

    chore: 불필요한 로직 삭제

--- apps/client/app/(beforeLogin)/social-login/redirect/page.tsx
+++ apps/client/app/(beforeLogin)/social-login/redirect/page.tsx
@@ -1,18 +1,8 @@
-import { fetcher } from "@wow-class/utils";
 import { dashboardApi } from "apis/dashboardApi";
 import { routePath } from "constants/routePath";
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 const AuthServerRedirectPage = async () => {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-
-  fetcher.setDefaultHeaders({
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${accessToken}`,
-  });
-
   const { memberRole, currentRecruitmentOpen } =
     await dashboardApi.getDashboardInfo();
 
