34025f7daa6d7898f451fa9a89bed04cb1d8d428
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-13 22:57:46 +0900

    feat: middleware에서 로그인 여부 및 멤버 롤도 확인하도록 기능 구현

--- apps/client/middleware.ts
+++ apps/client/middleware.ts
@@ -1,26 +1,23 @@
+import { dashboardApi } from "apis/dashboardApi";
 import { cookies } from "next/headers";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers);
-
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-
-  if (accessToken) {
-    requestHeaders.set("Authorization", `Bearer ${accessToken}`);
-  }
-
-  requestHeaders.set("Content-Type", "application/json");
-
-  return NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
-}
-
 export const config = {
   matcher: ["/my-page/:path*", "/my-study/:path*", "/study-apply/:path*"],
 };
+
+const middleware = async (req: NextRequest) => {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("accessToken");
+
+  const { memberRole } = await dashboardApi.getDashboardInfo();
+
+  if (!accessToken || memberRole !== "REGULAR") {
+    return NextResponse.redirect(new URL("/auth", req.url));
+  }
+
+  return NextResponse.next();
+};
+
+export default middleware;
