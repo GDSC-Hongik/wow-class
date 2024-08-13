c9a0177154373255bc1d239f6e5be9a9566e8230
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-13 12:59:37 +0900

    feat: 랜딩 페이지 배경 이미지 추가

--- apps/client/app/(beforeLogin)/landing/page.tsx
+++ apps/client/app/(beforeLogin)/landing/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { css } from "@styled-system/css";
-import { Header } from "@wow-class/ui/client";
+import { Header } from "@wow-class/ui";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Button from "wowds-ui/Button";
 
@@ -16,15 +17,29 @@ const LandingPage = () => {
     <div className={containerStyle}>
       <Header />
       <main className={mainContentStyle}>
-        <h1 className={taglineStyle}>
-          와우클래스와 함께 <br />
-          GDSC에서 스터디해요!
-        </h1>
+        <div className={headlineContainerStyle}>
+          <Image
+            alt="landing-background-image"
+            className={backgroundImageStyle}
+            height={264}
+            layout="intrinsic"
+            src="/images/landing-background.svg"
+            width={884}
+          />
+          <h1 className={headlineStyle}>
+            와우클래스와 함께 <br />
+            GDSC에서 스터디해요!
+          </h1>
+        </div>
         <p className={descriptionStyle}>
           GDSC Hongik은 홍익대학교의 학생 개발자들을 위해 <br />
           개발에 쉽게 입문할 수 있는 기회를 제공해요.
         </p>
-        <Button size="lg" onClick={handleClickLogin}>
+        <Button
+          aria-label="로그인 페이지로 이동"
+          size="lg"
+          onClick={handleClickLogin}
+        >
           로그인
         </Button>
       </main>
@@ -49,17 +64,32 @@ const mainContentStyle = css({
   height: "100%",
 });
 
-const taglineStyle = css({
+const headlineContainerStyle = css({
+  position: "relative",
+});
+
+const backgroundImageStyle = css({
+  margin: "auto",
+  position: "absolute",
+  minWidth: "884px",
+  top: -40,
+  left: -55,
+});
+
+const headlineStyle = css({
   color: "#17191C",
   fontSize: "74px",
   fontWeight: 800,
   lineHeight: "145%",
   letterSpacing: "-0.74px",
   marginBottom: "40px",
+  zIndex: 1,
+  position: "relative",
 });
 
 const descriptionStyle = css({
   color: "sub",
   textStyle: "body0",
   marginBottom: "48px",
+  zIndex: 1,
 });
