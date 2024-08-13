f36bd29777fb6487e74ef6f4724d33a86537277d
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-13 19:26:47 +0900

    chore: routePath enum 대신 문자열 리터럴로 변경

--- apps/client/constants/routePath.ts
+++ apps/client/constants/routePath.ts
@@ -5,4 +5,4 @@ export const routePath = {
   ["auth-error-during-recruitment"]: "/auth-error-during-recruitment",
   ["auth-error-after-recruitment"]: "/auth-error-after-recruitment",
   onboarding: "https://onboarding.gdschongik.com",
-};
+} as const;
