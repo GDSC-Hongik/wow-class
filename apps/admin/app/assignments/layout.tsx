5ff0bbc9065a3d6c07fcfef6b0f74e4f1e7612b0
hamo-o <yheel369@gmail.com>
2024-08-13 20:54:22 +0900

    feat: 과제 페이지 기본 UI

---
+++ apps/admin/app/assignments/layout.tsx
@@ -0,0 +1,21 @@
+import { Flex } from "@styled-system/jsx";
+
+const AssignmentsLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <Flex
+      direction="column"
+      gap="3rem"
+      paddingX="6.25rem"
+      paddingY="3rem"
+      width="100%"
+    >
+      {children}
+    </Flex>
+  );
+};
+
+export default AssignmentsLayout;
