ed331cdf080c6ae83d921fa2781435d1dc51ad3e
hamo-o <yheel369@gmail.com>
2024-08-15 04:17:22 +0900

    fix: 텍스트필드 onChange 에러 임시 주석처리

--- apps/admin/components/assignments/AssignmentForm.tsx
+++ apps/admin/components/assignments/AssignmentForm.tsx
@@ -9,13 +9,13 @@ const AssignmentForm = () => {
   return (
     <Flex direction="column" gap="2.25rem">
       <TextField
-        {...register("title", { required: true })}
+        // {...register("title", { required: true })}
         label="과제 제목"
         maxLength={100}
         placeholder="Ex. HTTP 통신 코드 작성하기"
       />
       <TextField
-        {...register("descriptionLink", { required: true })}
+        // {...register("descriptionLink", { required: true })}
         label="과제 명세 링크"
         placeholder="https://example.com"
         // TODO: 링크 형식 validate
