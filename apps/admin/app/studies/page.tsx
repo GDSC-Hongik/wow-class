b96391383e3dc787be8110a4fb960dbdcf7c5d41
Eugene Kim <eugene02876@gmail.com>
2024-08-16 16:52:59 +0900

    fix: 폴더구조 변경

--- apps/admin/app/studies/page.tsx
+++ apps/admin/app/studies/page.tsx
@@ -1,14 +1,13 @@
 import { css } from "@styled-system/css";
 import { Flex } from "@styled-system/jsx";
-import CreateStudyButton from "components/create-study/CreateStudyButton";
-import SelectStudySemester from "components/create-study/SelectStudySemester";
+
+import CreateStudyButton from "./create-study/_components/CreateStudyButton";
 
 const StudiesPage = () => {
   return (
     <>
       <Flex align="center" justifyContent="space-between">
         <p className={css({ textStyle: "h1" })}>개설된 스터디</p>
-        <SelectStudySemester />
       </Flex>
       <CreateStudyButton />
     </>
