e2f6fe05214ae5f127c04ed45d1c6a40ccc0377d
Eugene Kim <67894159+eugene028@users.noreply.github.com>
2024-08-16 16:55:30 +0900

    [Feature]: 스터디 개설 페이지 레이아웃  (#21)
    * chore: lock 패키지 변경 사항 반영
    
    * chore: 중복된 Navbar 컴포넌트 삭제
    
    * feat: 개설 스터디 기본 잡기
    
    * feat: 스터디 개설 페이지 레이아웃
    
    * fix: 찌그러짐 방지용 minWidth 추가
    
    * feat: 스터디 개설하기 뷰 생성
    
    * fix: 개설된 스터디로 화면 옮기기
    
    * feat: auth 체크용 middleWare 생성
    
    * fix: utils fetcher 함수 export 경로 변경
    
    * feat: 스터디 생성하기 페이지 생성
    
    * fix: router handler 삭제
    
    * refactor: api path 상수로 수정
    
    * feat: isAdmin 함수 추가
    
    * fix: 코드리뷰 반영
    
    * fix: admin여부에 따라 스터디 생성 박스 보이지 않도록
    
    * chore: 패키지 설치
    
    * fix: 빌드에러 고치기
    
    * fix: isadmin 판별 로직 미들웨어에서 진행
    
    * feat: 역할 논리 다시
    
    * fix: 폴더명 변경
    
    * fix: createStudy 경로 변경
    
    * feat:wow-icons 추가
    
    * fix: 폴더구조 변경
    
    * fix: 폴더 복수형 변경
    
    ---------
    
    Co-authored-by: ghdtjgus76 <ghdtjgus76@naver.com>

--- apps/admin/app/studies/page.tsx
+++ apps/admin/app/studies/page.tsx
@@ -1,5 +1,17 @@
-const Studies = () => {
-  return <div>Studies</div>;
+import { css } from "@styled-system/css";
+import { Flex } from "@styled-system/jsx";
+
+import CreateStudyButton from "./create-study/_components/CreateStudyButton";
+
+const StudiesPage = () => {
+  return (
+    <>
+      <Flex align="center" justifyContent="space-between">
+        <p className={css({ textStyle: "h1" })}>개설된 스터디</p>
+      </Flex>
+      <CreateStudyButton />
+    </>
+  );
 };
 
-export default Studies;
+export default StudiesPage;
