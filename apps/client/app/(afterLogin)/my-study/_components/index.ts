6ba77d1978da25a89a33f74db4df105ac24ed940
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-16 23:42:31 +0900

    feat: client afterLogin 배럴 파일 추가

---
+++ apps/client/app/(afterLogin)/my-study/_components/index.ts
@@ -0,0 +1,4 @@
+export { default as DailyTasks } from "./DailyTasks";
+export { default as Header } from "./Header";
+export { default as StudyCurriculum } from "./StudyCurriculum";
+export { default as StudyNotices } from "./StudyNotices";
