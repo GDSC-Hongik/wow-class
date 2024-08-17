698130e2f88c58247759806d453e4693d6c5e52d
SeieunYoo <isho48239@gmail.com>
2024-08-17 16:27:44 +0900

    feat: padWithZero export

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,4 @@
 export { default as fetcher } from "./fetcher";
+export { default as padWithZero } from "./padWithZero";
 export { default as parseDate } from "./parseDate";
 export { default as splitTime } from "./splitTime";
