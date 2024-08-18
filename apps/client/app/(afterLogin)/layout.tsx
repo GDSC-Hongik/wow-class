bc1ddaf4bb88dbb5c06bb42e20dff4e7e27bc7ec
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-18 22:21:14 +0900

    design: client layout width 변경

--- apps/client/app/(afterLogin)/layout.tsx
+++ apps/client/app/(afterLogin)/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <styled.main padding="54px 101px 35px 101px" width="100vw">
+      <styled.main padding="54px 101px 35px 101px" width="calc(100vw - 351px)">
         {children}
       </styled.main>
     </>
