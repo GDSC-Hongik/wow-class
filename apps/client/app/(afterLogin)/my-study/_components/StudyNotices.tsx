1cea5a5f8a5df015200029033c2bfd965601c7c3
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-19 03:23:54 +0900

    feat: 스터디 공지 목데이터 기반 UI 구축

--- apps/client/app/(afterLogin)/my-study/_components/StudyNotices.tsx
+++ apps/client/app/(afterLogin)/my-study/_components/StudyNotices.tsx
@@ -1,7 +1,23 @@
 import { css } from "@styled-system/css";
-import { Flex } from "@styled-system/jsx";
 import { Text } from "@wow-class/ui";
+import { formatISODateWithDot } from "@wow-class/utils";
+import Link from "next/link";
 import { color } from "wowds-tokens";
+
+const mockData = [
+  {
+    studyAnnounceId: 0,
+    title: "2주차 과제 검사 결과 안내",
+    link: "",
+    createdDate: "2024-08-18T17:13:29.913Z",
+  },
+  {
+    studyAnnounceId: 1,
+    title: "1주차 실습",
+    link: "",
+    createdDate: "2024-08-18T17:13:29.913Z",
+  },
+];
 
 const StudyNotices = () => {
   return (
@@ -9,30 +25,24 @@ const StudyNotices = () => {
       <Text as="h2" className={studyNoticeHeadingStyle} typo="h2">
         스터디 공지
       </Text>
-      <Flex
-        className={studyNoticeBoxStyle}
-        justifyContent="space-between"
-        style={{ backgroundColor: `${color.monoBackgroundPressed}` }}
-      >
-        <Text as="h3" typo="h3">
-          2주차 과제 검사 결과 안내
-        </Text>
-        <Text as="h3" typo="h3">
-          2024.06.08
-        </Text>
-      </Flex>
-      <Flex
-        className={studyNoticeBoxStyle}
-        justifyContent="space-between"
-        style={{ backgroundColor: "white" }}
-      >
-        <Text as="h3" typo="h3">
-          1주차 실습
-        </Text>
-        <Text as="h3" typo="h3">
-          2024.06.01
-        </Text>
-      </Flex>
+      {mockData.map(({ studyAnnounceId, title, link, createdDate }, index) => (
+        <Link
+          className={studyNoticeBoxStyle}
+          href={link}
+          key={studyAnnounceId}
+          style={{
+            backgroundColor:
+              index === 0 ? `${color.monoBackgroundPressed}` : "white",
+          }}
+        >
+          <Text as="h3" typo="h3">
+            {title}
+          </Text>
+          <Text as="h3" typo="h3">
+            {formatISODateWithDot(createdDate)}
+          </Text>
+        </Link>
+      ))}
     </section>
   );
 };
@@ -48,4 +58,6 @@ const studyNoticeBoxStyle = css({
   borderRadius: "4px",
   height: "80px",
   padding: "0 47px 0 20px",
+  display: "flex",
+  justifyContent: "space-between",
 });
--- packages/utils/src/formatDate.ts
+++ packages/utils/src/formatDate.ts
@@ -1,8 +1,9 @@
 /**
- *
  * @description ISO Date 형식의 string 을 hours, minutes, seconds, year, month, day 로 파싱합니다.
  * @example parseDate("2021-08-31T00:00:00.000Z") -> { year: 2021, month: 8, day: 31, hours: 0, minutes: 0, seconds: 0 }
  */
+
+import { padWithZero } from ".";
 
 export const parseISODate = (dateString: string) => {
   const date = new Date(dateString);
@@ -15,4 +16,19 @@ export const parseISODate = (dateString: string) => {
     minutes: date.getMinutes(),
     seconds: date.getSeconds(),
   };
+};
+
+/**
+ * @description ISO Date 형식의 string 을 "YYYY.MM.DD" 형식으로 포맷합니다.
+ * @example formatCreatedDate("2021-08-31T00:00:00.000Z") -> "2021.08.31"
+ */
+
+export const formatISODateWithDot = (dateString: string): string => {
+  const date = new Date(dateString);
+
+  const year = date.getFullYear();
+  const month = padWithZero(date.getMonth() + 1);
+  const day = padWithZero(date.getDate());
+
+  return `${year}.${month}.${day}`;
 };
