f1b88897d525fc3603ec79cd49ffc043431e1091
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-18 13:45:54 +0900

    chore: 불필요한 헤더 주입 로직 삭제

--- apps/client/apis/dashboardApi.ts
+++ apps/client/apis/dashboardApi.ts
@@ -1,22 +1,14 @@
 import { fetcher } from "@wow-class/utils";
 import { apiPath } from "constants/apiPath";
 import { tags } from "constants/tags";
-import { cookies } from "next/headers";
 import type { DashboardApiResponseDto } from "types/dtos/auth";
 
 export const dashboardApi = {
   getDashboardInfo: async () => {
-    const cookieStore = cookies();
-    const accessToken = cookieStore.get("accessToken")?.value;
-
-    // NOTE: middleware에서 호출하기 위해서 별도로 헤더 주입
     const response = await fetcher.get<DashboardApiResponseDto>(
       apiPath.dashboard,
       {
         next: { tags: [tags.dashboard] },
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
       }
     );
 
