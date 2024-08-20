c11492dc1b163ffda0f484afba4563b9b4607320
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-21 00:32:40 +0900

    chore: 모달 컴포넌트 기본 onClose는 router.back으로 지정하도록 수정

--- packages/ui/src/components/Modal/index.tsx
+++ packages/ui/src/components/Modal/index.tsx
@@ -3,6 +3,7 @@
 import { css } from "@styled-system/css";
 import { Flex, styled } from "@styled-system/jsx";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import type { CSSProperties, PropsWithChildren } from "react";
 
 import closeUrl from "../../assets/images/close.svg";
@@ -11,18 +12,24 @@ import { useClickOutside } from "../../hooks";
 /**
  * @description 모달 컴포넌트입니다.
  *
- * @param {() => void} onClose - 모달 컴포넌트를 닫기 위한 함수.
+ * @param {() => void} [onClose] - 모달 컴포넌트를 닫기 위한 함수.
  * @param {ReactNode} [children] - 모달 컴포넌트에 들어갈 자식 요소.
+ * @param {CSSProperties} [style] - 커스텀 스타일을 적용하기 위한 객체.
+ * @param {string} [className] - 커스텀 클래스를 적용하기 위한 문자열.
  */
 
 interface ModalProps extends PropsWithChildren {
-  onClose: () => void;
+  onClose?: () => void;
   style?: CSSProperties;
   className?: string;
 }
 
 const Modal = ({ children, onClose, ...rest }: ModalProps) => {
-  const modal = useClickOutside<HTMLDialogElement>(onClose);
+  const router = useRouter();
+
+  const handleClose = onClose || router.back;
+
+  const modal = useClickOutside<HTMLDialogElement>(handleClose);
 
   return (
     <Flex alignItems="center" className={backDropStyle} justifyContent="center">
@@ -33,7 +40,7 @@ const Modal = ({ children, onClose, ...rest }: ModalProps) => {
           height={24}
           src={closeUrl}
           width={24}
-          onClick={onClose}
+          onClick={handleClose}
         />
         {children}
       </styled.dialog>
