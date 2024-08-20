a2939bfa5013079b1f372dc73517d5f82d60dabe
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-21 00:00:27 +0900

    feat: 출석체크 모달 기능 추가

---
+++ apps/client/app/(afterLogin)/my-study/@modal/(.)check-attendance/[id]/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { css } from "@styled-system/css";
+import { Flex } from "@styled-system/jsx";
+import { Modal, Text } from "@wow-class/ui";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Button from "wowds-ui/Button";
+import TextField from "wowds-ui/TextField";
+
+const CheckAttendanceModal = () => {
+  const [error] = useState(false);
+  const [attendanceNumber, setAttendanceNumber] = useState("");
+
+  const router = useRouter();
+
+  const handleChangeAttendanceNumber = (value: string) => {
+    setAttendanceNumber(value);
+  };
+
+  const handleClickCheckAttendanceButton = () => {
+    // TODO api 요청 및 에러 처리 필요
+  };
+
+  const handleCloseModal = () => {
+    router.back();
+  };
+
+  return (
+    <Modal onClose={handleCloseModal}>
+      <Flex alignItems="center" direction="column" gap="sm" marginBottom="40px">
+        <section
+          aria-label="check-attendance-title"
+          className={checkAttendanceTitleStyle}
+        >
+          <Text as="h1" typo="h1">
+            기초 웹스터디
+          </Text>
+          <Image
+            alt="item separator"
+            height={6}
+            src="/images/dot.svg"
+            width={6}
+          />
+          <Text as="h1" typo="h1">
+            4주차
+          </Text>
+        </section>
+        <section
+          aria-label="check-attendance-description"
+          className={checkAttendanceDescriptionStyle}
+        >
+          <Text as="p" color="sub" typo="body1">
+            스터디 시작 후 멘토의 안내에 따라 출결번호를 입력해주세요.
+          </Text>
+          <Text as="p" color="error" typo="body1">
+            2024년 5월 23일 0:00 - 23:59까지
+          </Text>
+        </section>
+      </Flex>
+      <TextField
+        error={error}
+        helperText={error ? textfieldHelperText : ""}
+        label="출결번호 입력"
+        placeholder="Ex. 0000"
+        style={textfieldStyle}
+        value={attendanceNumber}
+        onChange={handleChangeAttendanceNumber}
+      />
+      <Button onClick={handleClickCheckAttendanceButton}>출석 체크하기</Button>
+    </Modal>
+  );
+};
+
+export default CheckAttendanceModal;
+
+const textfieldHelperText = <div>• 출석 실패! 출결번호를 확인해주세요.</div>;
+
+const checkAttendanceTitleStyle = css({
+  display: "flex",
+  gap: "sm",
+});
+
+const checkAttendanceDescriptionStyle = css({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "xxs",
+});
+
+const textfieldStyle = {
+  height: "89px",
+  marginBottom: "20px",
+};
--- apps/client/constants/routePath.ts
+++ apps/client/constants/routePath.ts
@@ -5,5 +5,6 @@ export const routePath = {
   ["my-assignment"]: "/my-study/my-assignment",
   ["auth-error-during-recruitment"]: "/auth-error-during-recruitment",
   ["auth-error-after-recruitment"]: "/auth-error-after-recruitment",
+  ["check-attendance"]: "/my-study/check-attendance",
   onboarding: "https://onboarding.gdschongik.com",
 } as const;
