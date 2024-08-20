5778fdd71d31ebe6bacb69188713dfbcde1ee995
SeieunYoo <isho48239@gmail.com>
2024-08-20 18:11:33 +0900

    chore: 얼리리턴으로 변경

--- apps/client/app/(afterLogin)/my-study/my-assignment/_components/AssignmentHistroy/index.tsx
+++ apps/client/app/(afterLogin)/my-study/my-assignment/_components/AssignmentHistroy/index.tsx
@@ -6,41 +6,41 @@ import Image from "next/image";
 import { AssignmentHistoryItem } from "./AssignmentHistoryItem";
 
 export const AssignmentHistory = async () => {
-  //const studyHistory = await studyHistoryApi.getStudyHistory(1);
+  // const studyHistory = await studyHistoryApi.getStudyHistory(1);
   const studyHistories = history;
+
+  if (studyHistories.length === 0) {
+    return (
+      <>
+        <Space height={133} />
+        <Flex alignItems="center" direction="column" gap="xl">
+          <Image
+            alt="no-history"
+            height={140}
+            src="/images/empty-history.png"
+            width={186}
+          />
+          <Text>진행된 과제가 없어요</Text>
+        </Flex>
+        <Space height={94} />
+      </>
+    );
+  }
 
   return (
     <>
-      {studyHistories.length ? (
-        <>
-          <Text as="h2" typo="h2">
-            과제 히스토리
-          </Text>
-          <Text color="sub" typo="body1">
-            지난 과제의 제출 내역을 확인해요.
-          </Text>
-          {studyHistories.map((history) => (
-            <AssignmentHistoryItem
-              history={history}
-              key={history.assignmentHistoryId}
-            />
-          ))}
-        </>
-      ) : (
-        <>
-          <Space height={133} />
-          <Flex alignItems="center" direction="column" gap="xl">
-            <Image
-              alt="no-history"
-              height={140}
-              src="/images/empty-history.png"
-              width={186}
-            />
-            <Text>진행된 과제가 없어요</Text>
-          </Flex>
-          <Space height={94} />
-        </>
-      )}
+      <Text as="h2" typo="h2">
+        과제 히스토리
+      </Text>
+      <Text color="sub" typo="body1">
+        지난 과제의 제출 내역을 확인해요.
+      </Text>
+      {studyHistories.map((history) => (
+        <AssignmentHistoryItem
+          history={history}
+          key={history.assignmentHistoryId}
+        />
+      ))}
     </>
   );
 };
