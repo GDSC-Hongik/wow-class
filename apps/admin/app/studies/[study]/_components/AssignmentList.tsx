7ff93adbaeb76bd84c844ba6f4e06d19854af3e2
Eugene Kim <eugene02876@gmail.com>
2024-08-20 21:04:19 +0900

    feat: 출석 보기

---
+++ apps/admin/app/studies/[study]/_components/AssignmentList.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@wow-class/ui";
+const AssignmentList = () => {
+  return (
+    <section>
+      <Text>주차별 과제</Text>
+    </section>
+  );
+};
+
+export default AssignmentList;
--- apps/admin/app/studies/[study]/page.tsx
+++ apps/admin/app/studies/[study]/page.tsx
@@ -1,6 +1,12 @@
+import CheckAttendanceNumber from "./_components/CheckAttendanceNumber";
 import Header from "./_components/Header";
 const Study = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <CheckAttendanceNumber />
+    </>
+  );
 };
 
 export default Study;
