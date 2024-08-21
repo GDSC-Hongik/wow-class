19c045830019f80fce6c06c802e17d95461e4b07
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-21 14:01:30 +0900

    chore: 불필요한 목데이터 삭제

--- apps/client/constants/mockData.ts
+++ apps/client/constants/mockData.ts
@@ -36,36 +36,6 @@ export const dailyTaskMockData: DailyTaskDataType[] = [
   },
 ];
 
-export const headerMockData = {
-  studyId: 1,
-  title: "기초 웹스터디",
-  academicYear: 2024,
-  semester: "FIRST",
-  studyType: "오프라인 세션",
-  notionLink: "string",
-  introduction: "새싹 개발자분들을 위한 개발 입문 스터디",
-  mentorName: "강가은",
-  dayOfWeek: "TUESDAY",
-  startTime: {
-    hour: 18,
-    minute: 0,
-    second: 0,
-    nano: 0,
-  },
-  endTime: {
-    hour: 19,
-    minute: 0,
-    second: 0,
-    nano: 0,
-  },
-  totalWeek: 8,
-  period: {
-    startDate: "2024-08-18T17:13:29.913Z",
-    endDate: "2024-08-18T17:13:29.913Z",
-    open: true,
-  },
-};
-
 export const studyCurriculumMockData: StudyCurriculumDataType[] = [
   {
     week: 1,
@@ -118,20 +88,5 @@ export const studyCurriculumMockData: StudyCurriculumDataType[] = [
     },
     attendanceStatus: "PENDING",
     assignmentSubmissionStatus: "FAILURE",
-  },
-];
-
-export const studyNoticesMockData = [
-  {
-    studyAnnounceId: 0,
-    title: "2주차 과제 검사 결과 안내",
-    link: "",
-    createdDate: "2024-08-21",
-  },
-  {
-    studyAnnounceId: 1,
-    title: "1주차 실습",
-    link: "",
-    createdDate: "2024-08-21",
   },
 ];
