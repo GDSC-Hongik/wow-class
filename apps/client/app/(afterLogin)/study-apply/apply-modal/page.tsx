7ef0d520462788fb6701ccc024bf0b50562433e0
SeieunYoo <isho48239@gmail.com>
2024-08-22 23:19:15 +0900

    chore: 이전 브랜치에 남은 로직 삭제

--- apps/client/app/(afterLogin)/study-apply/apply-modal/page.tsx
+++
@@ -1,8 +0,0 @@
-import { routePath } from "constants/routePath";
-import { redirect } from "next/navigation";
-
-const ApplyModalPage = () => {
-  return redirect(routePath["study-apply"]);
-};
-
-export default ApplyModalPage;
--- apps/client/app/(afterLogin)/study-apply/cancel-modal/page.tsx
+++
@@ -1,8 +0,0 @@
-import { routePath } from "constants/routePath";
-import { redirect } from "next/navigation";
-
-const CancelModalPage = () => {
-  return redirect(routePath["study-apply"]);
-};
-
-export default CancelModalPage;
