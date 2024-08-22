83f1fcb4a069b878e4fe9dd25c29aa044fae6092
SeieunYoo <101736358+SeieunYoo@users.noreply.github.com>
2024-08-22 18:12:00 +0900

    [Feature] 수강신청 페이지 모달 플로우 추가 (#45)
    * feat: 신청 모달, 취소 모달
    
    * feat: 컴포넌트 로직 분리
    
    * chore: 문구 수정
    
    * chore: 헤더 제거
    
    * chore: fetcher 주석 해제
    
    * feat: api 추가된 필드에 대해 dto 추가
    
    * feat: url 파람을 통해서 전달하지 않고 api fetch, 폴더 네이밍 변경
    
    * feat: StudyApplication, StudyCancel 로 수정, url로 받는 것으로 수정
    
    * fix: cancel 명사로 수정, StudyItem content min width 주기
    
    * chore: my-assignment 파일 위치 옮기기

--- apps/client/app/(afterLogin)/my-assignment/page.tsx
+++ apps/client/app/(afterLogin)/my-assignment/page.tsx
@@ -2,8 +2,7 @@ import { Flex, styled } from "@styled-system/jsx";
 import { Space } from "@wow-class/ui";
 import Image from "next/image";
 
-import { AssignmentHistory } from "@/(afterLogin)/my-study/my-assignment/_components";
-
+import { AssignmentHistory } from "./_components";
 import { AssignmentContent } from "./_components/AssignmentContent";
 
 const MyAssignmentPage = () => {
