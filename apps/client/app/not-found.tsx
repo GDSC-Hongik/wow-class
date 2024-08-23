e98b5feb9f435cb30546b24cd876415beaa6f508
ghdtjgus76 <ghdtjgus76@naver.com>
2024-08-23 19:06:27 +0900

    feat: client not-found 페이지 추가

---
+++ apps/client/app/not-found.tsx
@@ -0,0 +1,49 @@
+import { css } from "@styled-system/css";
+import { Flex } from "@styled-system/jsx";
+import { Header, Text } from "@wow-class/ui";
+import { routePath } from "constants/routePath";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "wowds-ui/Button";
+
+const NotFoundPage = () => {
+  return (
+    <Flex direction="column" minHeight="100vh">
+      <Header />
+      <main className={notFoundPageContentStyle}>
+        <Image
+          alt="not-found"
+          height={344}
+          src="/images/not-found.svg"
+          width={358}
+        />
+        <Flex direction="column" gap="md">
+          <Text as="h2" typo="display2">
+            오류가 발생했어요
+          </Text>
+          <Text color="sub" typo="body0">
+            요청하신 페이지를 찾을 수 없어요
+          </Text>
+        </Flex>
+        <Button asProp={Link} href={routePath.landing} style={buttonStyle}>
+          홈으로 이동
+        </Button>
+      </main>
+    </Flex>
+  );
+};
+
+export default NotFoundPage;
+
+const notFoundPageContentStyle = css({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "48px",
+  height: "100%",
+});
+
+const buttonStyle = {
+  width: 328,
+};
