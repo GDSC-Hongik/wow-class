aa73bac1f50dafa7d883cb367426b2e758b40f0c
Eugene Kim <eugene02876@gmail.com>
2024-08-24 02:35:40 +0900

    fix: timeType 추가

--- apps/admin/types/dtos/createStudy.ts
+++ apps/admin/types/dtos/createStudy.ts
@@ -1,6 +1,7 @@
 import type { DayOfWeekType } from "types/entities/dayofweek";
 import type { SemesterType } from "types/entities/semester";
 import type { StudyType } from "types/entities/study";
+import type { TimeType } from "types/entities/time";
 export interface CreateStudyApiRequestDto {
   mentorId: number;
   academicYear: number;
