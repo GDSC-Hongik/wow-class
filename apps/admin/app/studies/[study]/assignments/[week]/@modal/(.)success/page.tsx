b4409b61ed548a8a21fa3d2d995bfa1a663b16b3
hamo-o <yheel369@gmail.com>
2024-08-25 01:33:17 +0900

    feat: 모달에서 라우팅 시 모달 닫히도록 로직 추가

--- apps/admin/app/studies/[study]/assignments/[week]/@modal/(.)success/page.tsx
+++ apps/admin/app/studies/[study]/assignments/[week]/@modal/(.)success/page.tsx
@@ -3,6 +3,7 @@
 import { Flex, styled } from "@styled-system/jsx";
 import { Modal, Text } from "@wow-class/ui";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Button from "wowds-ui/Button";
 
 const AssignmentSuccessModal = ({
@@ -10,6 +11,11 @@ const AssignmentSuccessModal = ({
 }: {
   params: { study: string; week: string };
 }) => {
+  const pathname = usePathname();
+  if (!pathname.includes(`/${params.week}/success`)) {
+    return null;
+  }
+
   return (
     <Modal>
       <Flex align="center" direction="column" gap="2.375rem" width="21rem">
