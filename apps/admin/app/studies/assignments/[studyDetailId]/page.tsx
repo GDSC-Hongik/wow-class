1e785819b620c9fe4a233c80c94444fd4398f222
hamo-o <yheel369@gmail.com>
2024-08-27 22:19:51 +0900

    feat: 과제 내용 보기 페이지 API 연결

--- apps/admin/app/studies/assignments/[studyDetailId]/page.tsx
+++ apps/admin/app/studies/assignments/[studyDetailId]/page.tsx
@@ -1,44 +1,63 @@
 import { css } from "@styled-system/css";
 import { Flex, styled } from "@styled-system/jsx";
 import { Text } from "@wow-class/ui";
+import { padWithZero, parseISODate } from "@wow-class/utils";
+import { studyApi } from "apis/study/studyApi";
 import Link from "next/link";
 import Button from "wowds-ui/Button";
+import TextButton from "wowds-ui/TextButton";
 
-const Assignments = ({ params }: { params: { studyDetailId: string } }) => {
+const Assignments = async ({
+  params: { studyDetailId },
+}: {
+  params: { studyDetailId: string };
+}) => {
+  const assignment = await studyApi.getAssignment(studyDetailId);
+  // TODO: studyName 추가
+  const { week, title, descriptionLink, deadline } = assignment;
+  const { year, month, day, hours, minutes } = parseISODate(deadline);
+
   return (
     <>
       <styled.header className={headerStyle}>
         <Text as="h1" typo="h1">
-          {params.studyDetailId}주차 과제
+          {title} • {week}주차 과제
         </Text>
         <Flex gap="0.75rem">
           <Button size="sm" variant="outline">
             과제 휴강처리
           </Button>
-          <Link href={`${params.studyDetailId}/edit`}>
-            <Button size="sm" variant="outline">
-              수정
-            </Button>
-          </Link>
+          <Button
+            asProp={Link}
+            href={`${studyDetailId}/edit`}
+            size="sm"
+            variant="outline"
+          >
+            수정
+          </Button>
         </Flex>
       </styled.header>
       <Flex direction="column" gap="4rem">
         <Flex direction="column" gap="xxs">
           <Text typo="h2">과제 제목</Text>
           <Text color="sub" typo="body1">
-            과제 제목
+            {title}
           </Text>
         </Flex>
         <Flex direction="column" gap="xxs">
           <Text typo="h2">과제 명세 링크</Text>
-          <Text color="sub" typo="body1">
-            과제 명세 링크
-          </Text>
+          <TextButton
+            asProp={Link}
+            className={textButtonStyle}
+            href={descriptionLink}
+            target="_blank"
+            text={descriptionLink}
+          />
         </Flex>
         <Flex direction="column" gap="xxs">
           <Text typo="h2">과제 기한</Text>
           <Text color="sub" typo="body1">
-            과제 기한
+            {year}년 {month}월 {day}일 {hours}:{padWithZero(minutes)}
           </Text>
         </Flex>
       </Flex>
@@ -50,6 +69,10 @@ const headerStyle = css({
   width: "100%",
   display: "flex",
   justifyContent: "space-between",
+});
+
+const textButtonStyle = css({
+  color: "sub",
 });
 
 export default Assignments;
