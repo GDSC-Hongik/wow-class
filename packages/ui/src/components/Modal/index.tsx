c9f176a75a541db20ed9bcff2d9bb6d9deaada29
hamo-o <yheel369@gmail.com>
2024-08-28 20:18:53 +0900

    fix: 모달 컴포넌트 position 변경

--- packages/ui/src/components/Modal/index.tsx
+++ packages/ui/src/components/Modal/index.tsx
@@ -67,9 +67,10 @@ const backDropStyle = css({
   width: "100vw",
   height: "100vh",
 
-  position: "absolute",
+  position: "fixed",
   top: 0,
   left: 0,
+  zIndex: 9999,
 
   background: "backgroundDimmer",
 });
