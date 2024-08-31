ed440ccb8af2313aa4a1ae2a8d8cc681dbdebc53
hamo-o <yheel369@gmail.com>
2024-08-31 18:32:24 +0900

    feat: Picker 컴포넌트 도입

--- apps/admin/app/studies/assignments/[studyDetailId]/_components/AssignmentForm.tsx
+++ apps/admin/app/studies/assignments/[studyDetailId]/_components/AssignmentForm.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import { Flex } from "@styled-system/jsx";
+import { formatDateToISOString } from "@wow-class/utils";
+import { useEffect, useState } from "react";
 import { useFormContext } from "react-hook-form";
 import type {
   AssignmentApiRequestDto,
   AssignmentApiResponseDto,
 } from "types/dtos/assignmentList";
+import PickerGroup from "wowds-ui/PickerGroup";
+import SingleDatePicker from "wowds-ui/SingleDatePicker";
+import TimePicker from "wowds-ui/TimePicker";
 
 import CustomTextField from "./CustomTextField";
 
@@ -12,15 +19,25 @@ const AssignmentForm = ({
 }: {
   assignment: AssignmentApiResponseDto;
 }) => {
-  const { control } = useFormContext<AssignmentApiRequestDto>();
-  const { title, descriptionLink } = assignment;
+  const { control, setValue } = useFormContext<AssignmentApiRequestDto>();
+  const { title, descriptionLink, deadline } = assignment;
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(
+    deadline ? new Date(deadline) : undefined
+  );
 
-  // TODO: Picker 컴포넌트 추가
+  useEffect(() => {
+    if (selectedDate) {
+      setValue("deadLine", formatDateToISOString(selectedDate), {
+        shouldValidate: true,
+      });
+    }
+  }, [selectedDate, setValue]);
+
   return (
     <Flex direction="column" gap="2.25rem">
       <CustomTextField
         control={control}
-        defaultValue={title}
+        defaultValue={title || undefined}
         label="과제 제목"
         maxLength={100}
         name="title"
@@ -28,12 +45,19 @@ const AssignmentForm = ({
       />
       <CustomTextField
         control={control}
-        defaultValue={descriptionLink}
+        defaultValue={descriptionLink || undefined}
         label="과제 명세 링크"
         name="descriptionNotionLink"
         placeholder="https://example.com"
         // TODO: 링크 형식 validate
       />
+      <PickerGroup
+        selectedDate={selectedDate}
+        setSelectedDate={setSelectedDate}
+      >
+        <SingleDatePicker label="종료 날짜" />
+        <TimePicker label="종료 시간" />
+      </PickerGroup>
     </Flex>
   );
 };
