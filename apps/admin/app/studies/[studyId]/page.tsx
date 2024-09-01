e279505d3aa2387424901645160ae3e9aaac50b4
Eugene Kim <eugene02876@gmail.com>
2024-09-01 16:40:40 +0900

    feat: 수정 아이콘 추가

--- apps/admin/app/studies/[studyId]/page.tsx
+++ apps/admin/app/studies/[studyId]/page.tsx
@@ -1,6 +1,10 @@
+import { css } from "@styled-system/css";
 import { Flex } from "@styled-system/jsx";
 import { Space } from "@wow-class/ui";
 import { studyApi } from "apis/study/studyApi";
+import { routerPath } from "constants/router/routerPath";
+import Link from "next/link";
+import { Edit } from "wowds-icons";
 import Divider from "wowds-ui/Divider";
 
 import StudyAnnouncement from "./_components/announcement/StudyAnnouncement";
@@ -25,6 +29,9 @@ const StudyPage = ({ params }: { params: { studyId: string } }) => {
   return (
     <Flex direction="column" gap="64px">
       <Header studyId={studyId} />
+      <Link href={`${routerPath.studyDetailInfo.href}/${studyId}`}>
+        <Edit className={EditIconStyle} height={24} stroke="black" width={24} />
+      </Link>
       <CheckAttendanceNumber />
       <Divider style={MinHeightFullDividerStyle} />
       <AssignmentList studyId={studyId} />
@@ -42,3 +49,10 @@ export default StudyPage;
 const MinHeightFullDividerStyle = {
   minHeight: "1.2px",
 };
+
+const EditIconStyle = css({
+  position: "absolute",
+  right: "100px",
+  top: "57px",
+  cursor: "pointer",
+});
