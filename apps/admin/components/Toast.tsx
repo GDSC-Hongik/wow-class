3769a508d83896da51550ed3737cb9d58516d13a
hamo-o <yheel369@gmail.com>
2024-09-02 04:16:25 +0900

    feat: admin Toast 컴포넌트 구현

---
+++ apps/admin/components/Toast.tsx
@@ -0,0 +1,43 @@
+"use client";
+import type { ToastProps } from "@wow-class/ui";
+import { Toast as ToastUI } from "@wow-class/ui";
+import { useSetAtom } from "jotai";
+import { useEffect, useState } from "react";
+import { removeToastAtom } from "store";
+
+const TOAST_DURATION = 2000;
+const ANIMATION_DURATION = 200;
+
+const Toast = ({ id, ...rest }: ToastProps) => {
+  const [opacity, setOpacity] = useState<number>(0.2);
+  const removeToastItem = useSetAtom(removeToastAtom);
+
+  useEffect(() => {
+    setOpacity(1);
+    const timeoutForRemove = setTimeout(() => {
+      removeToastItem(id);
+    }, TOAST_DURATION);
+
+    const timeoutForVisible = setTimeout(() => {
+      setOpacity(0);
+    }, TOAST_DURATION - ANIMATION_DURATION);
+
+    return () => {
+      clearTimeout(timeoutForRemove);
+      clearTimeout(timeoutForVisible);
+    };
+  }, [id, removeToastItem]);
+
+  return (
+    <ToastUI
+      id={id}
+      style={{ opacity }}
+      transition="opacity"
+      transitionDelay="0.5"
+      transitionTimingFunction="ease-in-out"
+      {...rest}
+    />
+  );
+};
+
+export default Toast;
