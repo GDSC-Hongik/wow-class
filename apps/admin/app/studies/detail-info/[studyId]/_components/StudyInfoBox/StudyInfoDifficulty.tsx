69c4e175d04d6235249ff5a8dd1681511ceb5a89
Eugene Kim <eugene02876@gmail.com>
2024-09-01 16:48:25 +0900

    fix: 난이도 옆에 실제 값 반영되도록 수정

--- apps/admin/app/studies/detail-info/[studyId]/_components/StudyInfoBox/StudyInfoDifficulty.tsx
+++ apps/admin/app/studies/detail-info/[studyId]/_components/StudyInfoBox/StudyInfoDifficulty.tsx
@@ -3,11 +3,7 @@ import { Text } from "@wow-class/ui";
 import type { ReactNode } from "react";
 import { useState } from "react";
 import type { ControllerRenderProps, FieldValues } from "react-hook-form";
-import {
-  Controller,
-  ControllerFieldState,
-  useFormContext,
-} from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 import type { StudyDifficultyArrayType } from "types/entities/study";
 import { DownArrow } from "wowds-icons";
 import DropDown from "wowds-ui/DropDown";
@@ -20,7 +16,7 @@ export const difficultyArray: StudyDifficultyArrayType = [
 ];
 
 const StudyInfoDifficulty = ({ index }: { index: number }) => {
-  const [selectedOptionText, setSelectedOptionText] = useState("난이도");
+  const [selectedOptionText, setSelectedOptionText] = useState("");
   const { control } = useFormContext();
 
   const handleChangeStudyDifficulty = ({
@@ -45,7 +41,10 @@ const StudyInfoDifficulty = ({ index }: { index: number }) => {
           style={{ width: "100%" }}
           trigger={
             <div className={StudyDifficultySelectStyle}>
-              <Text color="sub">{selectedOptionText}</Text>
+              <div className={StudyDifficultySelectText}>
+                <Text color="sub">난이도</Text>
+                <Text color="textBlack">{selectedOptionText}</Text>
+              </div>
               <DownArrow height={20} stroke="sub" width={20} />
             </div>
           }
@@ -73,4 +72,9 @@ const StudyDifficultySelectStyle = css({
   padding: "xs",
   display: "flex",
   justifyContent: "space-between",
+});
+
+const StudyDifficultySelectText = css({
+  display: "flex",
+  gap: "8px",
 });
