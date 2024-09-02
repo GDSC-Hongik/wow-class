f7b96d869aed4a3cf42ed84285f7b76da29a577c
이현영 <89445100+hamo-o@users.noreply.github.com>
2024-09-02 19:19:18 +0900

    [Feature] 출결번호 API 연결 (#104)
    * feat: 출석 조회 API
    
    * feat: AttendaceItem 컴포넌트
    
    * fix: studyId 넘겨주는 방법 변경
    
    * feat: AttendaceList 컴포넌트
    
    * feat: 출결번호 상태체크 및 UI 렌더링
    
    * rename: statusmap 경로수정
    
    * rename: 파일명 타이포 수정
    
    * fix: 출결 상태에 따라 출결번호 색상 변경
    
    * fix: 비활성화 색상 변경

---
+++ apps/admin/app/studies/[studyId]/_components/attendance/AttendanceList.tsx
@@ -0,0 +1,27 @@
+import { Flex } from "@styled-system/jsx";
+import { Space, Text } from "@wow-class/ui";
+import { studyApi } from "apis/study/studyApi";
+import type { AttendanceApiResponseDto } from "types/dtos/attendance";
+
+import AttendanceItem from "./AttendanceItem";
+
+const AttendanceList = async ({ studyId }: { studyId: string }) => {
+  const attendanceList = await studyApi.getStudyAttendances(+studyId);
+
+  return (
+    <section>
+      <Text typo="h2">주차별 출결번호</Text>
+      <Space height={24} />
+      <Flex align="center" gap="md" overflow="scroll">
+        {attendanceList?.map((attendance: AttendanceApiResponseDto, index) => (
+          <AttendanceItem
+            attendance={attendance}
+            key={`${attendance.studyDetailId}-${index}`}
+          />
+        ))}
+      </Flex>
+    </section>
+  );
+};
+
+export default AttendanceList;
--- apps/admin/constants/tags.ts
+++ apps/admin/constants/tags.ts
@@ -5,4 +5,5 @@ export const enum tags {
   studyList = "studyList",
   studyBasicInfo = "studyBasicInfo",
   announcements = "announcements",
+  attendances = "attendances",
 }
