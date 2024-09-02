a5f4e8c63243a90da02757759aad5b3db6091166
ghdtjgus76 <ghdtjgus76@naver.com>
2024-09-03 01:21:06 +0900

    design: 스터디 공지 길어지는 경우 디자인 수정

--- apps/client/app/(afterLogin)/my-study/_components/StudyAnnouncementList.tsx
+++ apps/client/app/(afterLogin)/my-study/_components/StudyAnnouncementList.tsx
@@ -33,10 +33,10 @@ const StudyAnnouncementList = async () => {
                 index === 0 ? `${color.monoBackgroundPressed}` : "white",
             }}
           >
-            <Text as="h3" typo="h3">
+            <Text as="h3" className={studyAnnouncementTitleStyle} typo="h3">
               {title}
             </Text>
-            <Text as="h3" typo="h3">
+            <Text as="h3" className={studyAnnouncementDateStyle} typo="h3">
               {formatISODateWithDot(createdDate)}
             </Text>
           </Link>
@@ -56,7 +56,22 @@ const studyAnnouncementListBoxStyle = css({
   alignItems: "center",
   borderRadius: "4px",
   height: "80px",
-  padding: "0 47px 0 20px",
   display: "flex",
   justifyContent: "space-between",
+});
+
+const studyAnnouncementTitleStyle = css({
+  padding: "27px 36px 27px 20px",
+  width: "calc(100% - 182px)",
+  height: "100%",
+  whiteSpace: "nowrap",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+});
+
+const studyAnnouncementDateStyle = css({
+  padding: "27px 0",
+  height: "100%",
+  width: "182px",
+  textAlign: "center",
 });
