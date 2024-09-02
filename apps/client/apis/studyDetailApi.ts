e4e81a77fcc1dedfe382b1c1d66e8ec89a23960e
SeieunYoo <isho48239@gmail.com>
2024-09-02 23:11:53 +0900

    feat: 이번 주 과제 조회 스터디 추가

--- apps/client/apis/studyDetailApi.ts
+++ apps/client/apis/studyDetailApi.ts
@@ -1,7 +1,10 @@
 import { fetcher } from "@wow-class/utils";
 import { apiPath } from "constants/apiPath";
 import { tags } from "constants/tags";
-import type { StudyDetailDashboardDto } from "types/dtos/studyDetail";
+import type {
+  StudyDetailDashboardDto,
+  UpcomingStudyDto,
+} from "types/dtos/studyDetail";
 
 export const studyDetailApi = {
   getStudyDetailDashboard: async (studyId: number) => {
@@ -9,6 +12,16 @@ export const studyDetailApi = {
       `${apiPath.studyDetail}/dashboard?studyId=${studyId}`,
       {
         next: { tags: [tags.studyDetailDashboard] },
+      }
+    );
+
+    return response.data;
+  },
+  getUpcomingStudy: async (studyId: number) => {
+    const response = await fetcher.get<UpcomingStudyDto>(
+      `${apiPath.studyDetail}/upcoming?studyId=${studyId}`,
+      {
+        next: { tags: [tags.upcomingStudy] },
       }
     );
 
