de336da75461e494a356c2160f1c7388f812481b
Eugene Kim <eugene02876@gmail.com>
2024-09-02 16:12:52 +0900

    fix: 스터디 상세 보기 레이아웃 잡기

--- apps/admin/app/studies/[studyId]/_components/assignment/AssignmentListItem.tsx
+++ apps/admin/app/studies/[studyId]/_components/assignment/AssignmentListItem.tsx
@@ -21,7 +21,7 @@ const AssignmentListItem = ({
   return (
     <Table>
       <Table.Left style={TableLeftStyle}>
-        <Flex alignItems="center" gap="xxs">
+        <Flex alignItems="center" flex={1} gap="xxs" minWidth="50px">
           <div
             className={ThisWeekBarStyle({
               type: thisWeekAssignment ? "thisWeek" : "notThisWeek",
@@ -29,8 +29,10 @@ const AssignmentListItem = ({
           />
           <Text typo="body1">{week}주차</Text>
         </Flex>
-        <Flex direction="column" gap="xxs">
-          <Text typo="h3">{title || "-"}</Text>
+        <Flex direction="column" flex={2} gap="xxs">
+          <Text style={AssignmentTitleStyle} typo="h3">
+            {title || "-"}
+          </Text>
           <Text color="sub" typo="body2">
             {deadline ? studyDeadline : "-"}
           </Text>
@@ -68,4 +70,11 @@ const TableLeftStyle = {
   display: "flex",
   alignItems: "center",
   gap: "47px",
+};
+
+const AssignmentTitleStyle = {
+  whiteSpace: "nowrap",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  maxWidth: "680px",
 };
