7e9ceee1eaa5ca25080594502ed29d70d677997e
hamo-o <yheel369@gmail.com>
2024-09-08 03:43:00 +0900

    chore: 불필요한 typo prop 삭제, style 객체 분리

--- apps/admin/app/students/_components/StudentListItem.tsx
+++ apps/admin/app/students/_components/StudentListItem.tsx
@@ -2,6 +2,7 @@ import { css } from "@styled-system/css";
 import { styled } from "@styled-system/jsx";
 import { Text } from "@wow-class/ui";
 import Link from "next/link";
+import type { CSSProperties } from "react";
 import type { StudyStudentApiResponseDto } from "types/dtos/studyStudent";
 import TextButton from "wowds-ui/TextButton";
 
@@ -14,23 +15,23 @@ const StudentListItem = ({
 }: StudyStudentApiResponseDto) => {
   return (
     <styled.tr>
-      <Text as="td" className={tableThStyle} typo="body1">
+      <Text as="td" className={tableThStyle}>
         {name}
       </Text>
-      <Text as="td" className={tableThStyle} typo="body1">
+      <Text as="td" className={tableThStyle}>
         {studentId}
       </Text>
-      <Text as="td" className={tableThStyle} typo="body1">
+      <Text as="td" className={tableThStyle}>
         {discordUsername}
       </Text>
-      <Text as="td" className={tableThStyle} typo="body1">
+      <Text as="td" className={tableThStyle}>
         {nickname}
       </Text>
-      <Text as="td" className={tableThStyle} typo="body1">
+      <Text as="td" className={tableThStyle}>
         <TextButton
           asProp={Link}
           href={githubLink || ""}
-          style={{ width: "fit-content", padding: 0 }}
+          style={textButtonStyle}
           text={githubLink}
         />
       </Text>
@@ -41,5 +42,10 @@ const StudentListItem = ({
 const tableThStyle = css({
   padding: "1rem",
 });
+
+const textButtonStyle: CSSProperties = {
+  width: "fit-content",
+  padding: 0,
+};
 
 export default StudentListItem;
