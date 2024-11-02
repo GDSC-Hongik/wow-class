899423e7c7f1ea0f4e39586c5e324d925a3f223a
hamo-o <yheel369@gmail.com>
2024-11-02 22:33:29 +0900

    feat: 추가 및 변경된 DTO 등록(StudyStudentApiResponseDto, PageStudyStudentApiResponseDto)

--- apps/admin/types/dtos/studyStudent.ts
+++ apps/admin/types/dtos/studyStudent.ts
@@ -1,3 +1,5 @@
+import type { PageableObject, SortType } from "types/entities/page";
+
 export interface StudyStudentApiResponseDto {
   memberId: number;
   name: string;
@@ -5,4 +7,54 @@ export interface StudyStudentApiResponseDto {
   discordUsername: string;
   nickname: string;
   githubLink: string;
+  studyHistoryStatus: "NONE" | "COMPLETED";
+  isFirstRoundOutstandingStudent: boolean;
+  isSecondRoundOutstandingStudent: boolean;
+  studyTasks: StudyTaskResponseDto<"ATTENDANCE" | "ASSIGNMENT">[];
+  assignmentRate: number;
+  attendanceRate: number;
 }
+
+export interface PageStudyStudentApiResponseDto {
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  content: StudyStudentApiResponseDto[];
+  number: number;
+  sort: SortType;
+  numberOfElements: number;
+  pageable: PageableObject;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
+
+export interface StudyTaskResponseDtoBase {
+  studyDetailId: number;
+  week: number;
+  deadLine: string;
+  assignment: boolean;
+  attendance: boolean;
+}
+
+export interface AttendanceTask extends StudyTaskResponseDtoBase {
+  taskType: "ATTENDANCE";
+  attendanceStatus:
+    | "ATTENDED"
+    | "NOT_ATTENDED"
+    | "BEFORE_ATTENDANCE"
+    | "CANCELED";
+}
+
+export interface AssignmentTask extends StudyTaskResponseDtoBase {
+  taskType: "ASSIGNMENT";
+  assignmentTitle: string;
+  assignmentSubmissionStatus:
+    | "NOT_SUBMITTED"
+    | "FAILURE"
+    | "SUCCESS"
+    | "CANCELED";
+}
+
+export type StudyTaskResponseDto<T extends "ATTENDANCE" | "ASSIGNMENT"> =
+  T extends "ATTENDANCE" ? AttendanceTask : AssignmentTask;
---
+++ apps/admin/types/entities/page.ts
@@ -0,0 +1,22 @@
+export type SortType = {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+};
+
+export type PageableType = {
+  page: number;
+  size: number;
+  sort: [];
+};
+
+export type PageableObject = {
+  pageable: {
+    offset: number;
+    sort: SortType;
+    pageNumber: number;
+    pageSize: number;
+    unpaged: boolean;
+    paged: boolean;
+  };
+};
