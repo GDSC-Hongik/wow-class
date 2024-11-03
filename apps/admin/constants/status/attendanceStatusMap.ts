d7370db439699e74ff6f1f83251f2087ca3ec4bc
hamo-o <yheel369@gmail.com>
2024-11-03 20:20:55 +0900

    feat: 과제 출석 태그 맵 객체

--- apps/admin/constants/status/attendanceStatusMap.ts
+++ apps/admin/constants/status/attendanceStatusMap.ts
@@ -1,4 +1,5 @@
 import type { ComponentProps } from "react";
+import type { AttendanceStatusType } from "types/entities/attendance";
 import type Tag from "wowds-ui/Tag";
 
 export const attendanceStatusMap: Record<
@@ -7,4 +8,14 @@ export const attendanceStatusMap: Record<
 > = {
   ONGOING_ATTENDANCE: { label: "진행중", color: "blue" },
   BEFORE_ATTENDANCE: { label: "진행전", color: "grey" },
+};
+
+export const attendanceTaskStatusMap: Record<
+  AttendanceStatusType,
+  { tagText: string; tagColor: ComponentProps<typeof Tag>["color"] }
+> = {
+  BEFORE_ATTENDANCE: { tagText: "기간아님", tagColor: "grey" },
+  NOT_ATTENDED: { tagText: "미출석", tagColor: "red" },
+  ATTENDED: { tagText: "출석완료", tagColor: "blue" },
+  CANCELED: { tagText: "휴강", tagColor: "grey" },
 };
