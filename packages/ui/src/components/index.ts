a61d5f1bad0195c7b43658a99ecf4291a03ea7af
hamo-o <yheel369@gmail.com>
2024-11-03 19:18:07 +0900

    feat: 수료 체크박스 StarCheckIcon 컴포넌트

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -3,5 +3,6 @@ export { default as Header } from "./Header";
 export { default as Modal } from "./Modal";
 export { default as NavItem } from "./NavItem";
 export { default as Space } from "./Space";
+export { default as StarCheckIcon } from "./StarCheckIcon";
 export { default as Table } from "./Table";
 export { default as Text } from "./Text";
