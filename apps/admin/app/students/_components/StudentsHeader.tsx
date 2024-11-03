0ef67cf1ce741709656d0f68dfc621b234ed05a6
hamo-o <yheel369@gmail.com>
2024-11-04 03:01:42 +0900

    feat: 엑셀 다운로드 버튼

--- apps/admin/app/students/_components/StudentsHeader.tsx
+++ apps/admin/app/students/_components/StudentsHeader.tsx
@@ -1,6 +1,7 @@
-import { Flex } from "@styled-system/jsx";
+import { Flex, styled } from "@styled-system/jsx";
 import { Text } from "@wow-class/ui";
 import ItemSeparator from "components/ItemSeparator";
+import Image from "next/image";
 import type { CSSProperties } from "react";
 import type { StudyListApiResponseDto } from "types/dtos/studyList";
 
@@ -12,11 +13,19 @@ const StudentsHeader = ({
   studyList: StudyListApiResponseDto[];
 }) => {
   return (
-    <Flex paddingBottom="1.5rem">
+    <Flex justify="space-between" paddingBottom="1.5rem">
       <Text as="h1" style={titleStyle} typo="h1">
         수강생 관리 <ItemSeparator height={6} width={6} />
         <StudyDropDown studyList={studyList} />
       </Text>
+      <styled.button cursor="pointer">
+        <Image
+          alt="다운로드"
+          height={24}
+          src="/images/download.svg"
+          width={24}
+        />
+      </styled.button>
     </Flex>
   );
 };
