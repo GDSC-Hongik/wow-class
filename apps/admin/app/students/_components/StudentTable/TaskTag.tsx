c5b0770b810392758ec9ae50734f9a73f3ed1309
hamo-o <yheel369@gmail.com>
2024-11-10 16:51:39 +0900

    refactor: 타입에 따라 태그 정보를 반환하는 formatTaskToTagInfo 함수

--- apps/admin/app/students/_components/StudentTable/TaskTag.tsx
+++ apps/admin/app/students/_components/StudentTable/TaskTag.tsx
@@ -1,34 +1,29 @@
 import { assignmentSubmissionStatusMap } from "constants/status/assignmentStatusMap";
 import { attendanceTaskStatusMap } from "constants/status/attendanceStatusMap";
 import type { StudyTaskResponseDto } from "types/dtos/studyStudent";
+import type { TaskType } from "types/entities/task";
 import Tag from "wowds-ui/Tag";
 
-const TaskTag = ({
-  task,
-}: {
-  task: StudyTaskResponseDto<"ASSIGNMENT" | "ATTENDANCE">;
-}) => {
-  const { taskType } = task;
-  if (taskType === "ATTENDANCE") {
-    const { attendanceStatus } = task;
-    const { tagText, tagColor } = attendanceTaskStatusMap[attendanceStatus];
-    return (
-      <Tag color={tagColor} variant="solid2">
-        {tagText}
-      </Tag>
-    );
-  }
-  if (taskType === "ASSIGNMENT") {
-    const { assignmentSubmissionStatus } = task;
-    const { tagText, tagColor } =
-      assignmentSubmissionStatusMap[assignmentSubmissionStatus];
-    return (
-      <Tag color={tagColor} variant="solid2">
-        {tagText}
-      </Tag>
-    );
-  }
-  return null;
+const TaskTag = ({ task }: { task: StudyTaskResponseDto<TaskType> }) => {
+  const formatTaskToTagInfo = () => {
+    if (task.taskType === "ATTENDANCE") {
+      return attendanceTaskStatusMap[task.attendanceStatus];
+    }
+    if (task.taskType === "ASSIGNMENT") {
+      return assignmentSubmissionStatusMap[task.assignmentSubmissionStatus];
+    }
+    return null;
+  };
+
+  const tagInfo = formatTaskToTagInfo();
+  if (!tagInfo) return null;
+  const { tagText, tagColor } = tagInfo;
+
+  return (
+    <Tag color={tagColor} variant="solid2">
+      {tagText}
+    </Tag>
+  );
 };
 
 export default TaskTag;
