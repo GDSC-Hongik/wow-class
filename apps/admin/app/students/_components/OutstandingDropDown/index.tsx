c4187ef337994edd225c776106a86cc8f43a84db
hamo-o <yheel369@gmail.com>
2024-11-10 19:25:02 +0900

    refactor: 우수회원 옵션 상수 분리

--- apps/admin/app/students/_components/OutstandingDropDown/index.tsx
+++ apps/admin/app/students/_components/OutstandingDropDown/index.tsx
@@ -1,27 +1,14 @@
+"use client";
+
 import { styled } from "@styled-system/jsx";
+import {
+  OUTSTANDING_ADD_OPTIONS,
+  OUTSTANDING_DEL_OPTIONS,
+} from "constants/status/outstandigOptions";
 import DropDown from "wowds-ui/DropDown";
 import DropDownOption from "wowds-ui/DropDownOption";
 
 import DropDownTrigger from "./DropDownTrigger";
-
-type AchievementType =
-  | "FIRST_ROUND_OUTSTANDING_STUDENT"
-  | "SECOND_ROUND_OUTSTANDING_STUDENT";
-
-type OutstandingDropDownOption = {
-  id: number;
-  text: string;
-  value: AchievementType;
-};
-
-const OUTSTANDING_ADD_OPTIONS: OutstandingDropDownOption[] = [
-  { id: 1, text: "1차 우수 처리", value: "FIRST_ROUND_OUTSTANDING_STUDENT" },
-  { id: 2, text: "2차 우수 처리", value: "SECOND_ROUND_OUTSTANDING_STUDENT" },
-];
-const OUTSTANDING_DEL_OPTIONS: OutstandingDropDownOption[] = [
-  { id: 1, text: "1차 우수 철회", value: "FIRST_ROUND_OUTSTANDING_STUDENT" },
-  { id: 2, text: "2차 우수 철회", value: "SECOND_ROUND_OUTSTANDING_STUDENT" },
-];
 
 const OutstandingDropDown = ({ type }: { type: "ADD" | "DEL" }) => {
   const findOptions = () => {
---
+++ apps/admin/constants/status/outstandigOptions.ts
@@ -0,0 +1,18 @@
+export type AchievementType =
+  | "FIRST_ROUND_OUTSTANDING_STUDENT"
+  | "SECOND_ROUND_OUTSTANDING_STUDENT";
+
+export type OutstandingDropDownOption = {
+  id: number;
+  text: string;
+  value: AchievementType;
+};
+
+export const OUTSTANDING_ADD_OPTIONS: OutstandingDropDownOption[] = [
+  { id: 1, text: "1차 우수 처리", value: "FIRST_ROUND_OUTSTANDING_STUDENT" },
+  { id: 2, text: "2차 우수 처리", value: "SECOND_ROUND_OUTSTANDING_STUDENT" },
+];
+export const OUTSTANDING_DEL_OPTIONS: OutstandingDropDownOption[] = [
+  { id: 1, text: "1차 우수 철회", value: "FIRST_ROUND_OUTSTANDING_STUDENT" },
+  { id: 2, text: "2차 우수 철회", value: "SECOND_ROUND_OUTSTANDING_STUDENT" },
+];
