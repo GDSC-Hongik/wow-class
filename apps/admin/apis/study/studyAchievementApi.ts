a6568b2fbd08f5fc85be88ab9d5f683a10adb423
hamo-o <yheel369@gmail.com>
2024-11-10 20:41:54 +0900

    feat: 우수회원 처리, 철회 API 등록

---
+++ apps/admin/apis/study/studyAchievementApi.ts
@@ -0,0 +1,28 @@
+import { fetcher } from "@wow-class/utils";
+import type { OutstandingStudentApiRequestDto } from "types/dtos/outstandingStudent";
+
+const studyAchievementApi = {
+  postStudyAchievement: async (
+    studyId: number,
+    data: OutstandingStudentApiRequestDto
+  ) => {
+    const response = await fetcher.post(
+      `/mentor/study-achievements?studyId=${studyId}`,
+      data
+    );
+    return { success: response.ok };
+  },
+
+  deleteStudyAchievement: async (
+    studyId: number,
+    data: OutstandingStudentApiRequestDto
+  ) => {
+    const response = await fetcher.delete(
+      `/mentor/study-achievements?studyId=${studyId}`,
+      data
+    );
+    return { success: response.ok };
+  },
+};
+
+export default studyAchievementApi;
