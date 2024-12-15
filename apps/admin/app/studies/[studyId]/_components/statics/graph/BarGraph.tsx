2c0af2c60120e5bf8ece53d57499265f79d5872d
Eugene Kim <eugene02876@gmail.com>
2024-12-15 15:19:48 +0900

    feat: BarGraph 스타일 생성

--- apps/admin/app/studies/[studyId]/_components/statics/graph/BarGraph.tsx
+++ apps/admin/app/studies/[studyId]/_components/statics/graph/BarGraph.tsx
@@ -1,14 +1,33 @@
 import { css } from "@styled-system/css";
 
 const BarGraph = ({ totalStudentCount }: { totalStudentCount: number }) => {
-  return <div className={BarGraphBackgroundStyle}></div>;
+  return (
+    <div className={BarGraphBackgroundStyle}>
+      <div
+        className={BarGraphStyle}
+        style={{ width: `${totalStudentCount}px` }}
+      ></div>
+    </div>
+  );
 };
+
+export default BarGraph;
 
 const BarGraphBackgroundStyle = css({
   width: "232px",
+  height: "24px",
   display: "flex",
   alignItems: "center",
   cursor: "pointer",
   gap: "4px",
   flex: 2,
+  backgroundColor: "monoBackgroundPressed",
+});
+
+const BarGraphStyle = css({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  backgroundColor: "primary",
+  height: "24px",
 });
