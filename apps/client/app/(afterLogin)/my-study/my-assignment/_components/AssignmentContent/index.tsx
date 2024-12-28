e37446dc93abd66ccdaa0003554f213355745a20
SeieunYoo <isho48239@gmail.com>
2024-12-28 22:27:16 +0900

    feat: 1주차 과제가 있으면  레포박스 왼쪽에 위치하도록 수정

--- apps/client/app/(afterLogin)/my-study/my-assignment/_components/AssignmentContent/index.tsx
+++ apps/client/app/(afterLogin)/my-study/my-assignment/_components/AssignmentContent/index.tsx
@@ -33,17 +33,22 @@ export const AssignmentContent = async () => {
     );
   }
 
+  const isAnyFirstWeekAssignment = studyDashboard.submittableAssignments.some(
+    ({ week }) => week === 1
+  );
   return (
     <section>
-      <Flex className={boxContainerStyle} gap="lg">
-        {studyDashboard.isLinkEditable && (
-          <RepositorySubmissionBox
-            repositoryLink={studyDashboard.repositoryLink}
-          />
-        )}
+      <Flex
+        className={boxContainerStyle}
+        flexDirection={isAnyFirstWeekAssignment ? "row-reverse" : "row"}
+        gap="lg"
+      >
         <AssignmentOverviewBox
           assignments={studyDashboard.submittableAssignments}
           buttonsDisabled={!studyDashboard.repositoryLink}
+          repositoryLink={studyDashboard.repositoryLink}
+        />
+        <RepositorySubmissionBox
           repositoryLink={studyDashboard.repositoryLink}
         />
       </Flex>
