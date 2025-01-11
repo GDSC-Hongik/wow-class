bb3c838e3e474829903eb0eab6604713c0e60172
SeieunYoo <isho48239@gmail.com>
2025-01-11 21:54:28 +0900

    feat: 목데이터 삭제 및 empty ui 추가

--- apps/client/app/(afterLogin)/(mobile)/mobile/announcement/page.tsx
+++ apps/client/app/(afterLogin)/(mobile)/mobile/announcement/page.tsx
@@ -5,28 +5,6 @@ import { formatISODateWithDot } from "@wow-class/utils";
 import { myStudyApi } from "apis/myStudyApi";
 import Image from "next/image";
 import Link from "next/link";
-import type { StudyAnnouncementListDtoType } from "types/dtos/myStudy";
-
-const mockStudyAnnouncementListData: StudyAnnouncementListDtoType = [
-  {
-    studyAnnounceId: 1,
-    title: "Welcome to the Study Group!",
-    link: "https://example.com/announcement/1",
-    createdDate: "2025-01-10T10:00:00Z",
-  },
-  {
-    studyAnnounceId: 2,
-    title: "Exam Schedule Released",
-    link: "https://example.com/announcement/2",
-    createdDate: "2025-01-12T14:00:00Z",
-  },
-  {
-    studyAnnounceId: 3,
-    title: "New Study Materials Available",
-    link: "https://example.com/announcement/3",
-    createdDate: "2025-01-15T09:30:00Z",
-  },
-];
 
 const MobileStudyAnnouncementPage = async () => {
   const myOngoingStudyInfoData = await myStudyApi.getMyOngoingStudyInfo();
@@ -39,34 +17,34 @@ const MobileStudyAnnouncementPage = async () => {
     myOngoingStudyInfoData?.studyId
   );
 
-  const data = mockStudyAnnouncementListData;
-
   return (
     <section aria-label="study-announcement-list">
       <Text as="h1" className={studyAnnouncementListHeadingStyle} typo="h1">
         스터디 공지
       </Text>
       <Flex direction="column" gap="12px" justifyContent="center">
-        {data?.length ? (
-          data?.map(({ studyAnnounceId, title, link, createdDate }) => (
-            <Link
-              className={studyAnnouncementListBoxStyle}
-              href={link}
-              key={studyAnnounceId}
-              target="_blank"
-            >
-              <Text
-                as="span"
-                className={studyAnnouncementTitleStyle}
-                typo="body2"
+        {studyAnnouncementListData?.length ? (
+          studyAnnouncementListData?.map(
+            ({ studyAnnounceId, title, link, createdDate }) => (
+              <Link
+                className={studyAnnouncementListBoxStyle}
+                href={link}
+                key={studyAnnounceId}
+                target="_blank"
               >
-                {title}
-              </Text>
-              <Text as="span" color="sub" typo="body3">
-                {formatISODateWithDot(createdDate)}
-              </Text>
-            </Link>
-          ))
+                <Text
+                  as="span"
+                  className={studyAnnouncementTitleStyle}
+                  typo="body2"
+                >
+                  {title}
+                </Text>
+                <Text as="span" color="sub" typo="body3">
+                  {formatISODateWithDot(createdDate)}
+                </Text>
+              </Link>
+            )
+          )
         ) : (
           <Flex alignItems="center" direction="column" gap={24} paddingY={38}>
             <Image
--- apps/client/app/(afterLogin)/(mobile)/mobile/my-study/_components/DailyTasks.tsx
+++ apps/client/app/(afterLogin)/(mobile)/mobile/my-study/_components/DailyTasks.tsx
@@ -1,49 +1,9 @@
 import { Flex } from "@styled-system/jsx";
 import { Space, Text } from "@wow-class/ui";
 import { myStudyApi } from "apis/myStudyApi";
-import type { DailyTaskListDtoType } from "types/dtos/myStudy";
-import type { DailyTaskType } from "types/entities/myStudy";
+import Image from "next/image";
 
 import DailyTaskItem from "./DailyTaskItem";
-
-const mockAssignmentTaskData: DailyTaskListDtoType<DailyTaskType> = [
-  {
-    studyDetailId: 1,
-    week: 2,
-    taskType: "ATTENDANCE",
-    deadLine: "2025-01-15T23:59:59Z",
-    attendanceStatus: "BEFORE_ATTENDANCE", // Example value, assuming "PRESENT" is part of AttendanceStatusType
-    assignmentSubmissionStatus: "SUCCESS",
-    assignmentTitle: "sdf",
-  },
-  {
-    studyDetailId: 2,
-    week: 3,
-    taskType: "ASSIGNMENT",
-    deadLine: "2025-01-20T23:59:59Z",
-    assignmentTitle: "Math Homework 1",
-    attendanceStatus: "ATTENDED",
-    assignmentSubmissionStatus: "SUCCESS", // Must be either "FAILURE" or "SUCCESS
-  },
-  {
-    studyDetailId: 2,
-    week: 3,
-    taskType: "ASSIGNMENT",
-    deadLine: "2025-01-20T23:59:59Z",
-    assignmentTitle: "Math Homework 1",
-    attendanceStatus: "ATTENDED",
-    assignmentSubmissionStatus: "SUCCESS", // Must be either "FAILURE" or "SUCCESS
-  },
-  {
-    studyDetailId: 2,
-    week: 3,
-    taskType: "ASSIGNMENT",
-    deadLine: "2025-01-20T23:59:59Z",
-    assignmentTitle: "Math Homework 1",
-    attendanceStatus: "ATTENDED",
-    assignmentSubmissionStatus: "SUCCESS", // Must be either "FAILURE" or "SUCCESS
-  },
-];
 
 const DailyTasks = async () => {
   const myOngoingStudyData = await myStudyApi.getMyOngoingStudyInfo();
@@ -56,19 +16,32 @@ const DailyTasks = async () => {
     myOngoingStudyData?.studyId
   );
 
-  const data = mockAssignmentTaskData;
   return (
     <>
       <Text typo="h2">오늘의 할 일</Text>
       <Space height={14} />
       <Flex direction="column" gap="12px">
-        {data.map((dailyTask, index) => (
-          <DailyTaskItem
-            dailyTask={dailyTask}
-            index={index}
-            key={dailyTask.studyDetailId}
-          />
-        ))}
+        {dailyTaskData?.length ? (
+          dailyTaskData.map((dailyTask, index) => (
+            <DailyTaskItem
+              dailyTask={dailyTask}
+              index={index}
+              key={dailyTask.studyDetailId}
+            />
+          ))
+        ) : (
+          <Flex alignItems="center" direction="column" gap={24} paddingY={38}>
+            <Image
+              alt="empty-study"
+              height={140}
+              src="/images/empty.svg"
+              width={186}
+            />
+            <Text as="h2" color="sub" typo="h2">
+              아직 할 일이 없어요.
+            </Text>
+          </Flex>
+        )}
       </Flex>
     </>
   );
