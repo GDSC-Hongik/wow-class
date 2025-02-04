2803ac237e2ef84bcbf11a591698c803c386d40d
SeieunYoo <101736358+SeieunYoo@users.noreply.github.com>
2025-02-04 16:02:58 +0900

    Revert "[Deploy] 학생용 페이지 모바일 대응 (#172)"
    This reverts commit fc2746311448afbf12e4efec6c614b1a91ba4635.

--- apps/client/utils/isMobileUser.ts
+++
@@ -1,5 +0,0 @@
-export const isMobileUser = (userAgent: string) => {
-  const mobileRegex =
-    /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-  return mobileRegex.test(userAgent);
-};
