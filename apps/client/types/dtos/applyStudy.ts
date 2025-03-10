f9bf4ad7cc30f1493744ee18321706977c0b4c76
SeieunYoo <isho48239@gmail.com>
2025-03-11 00:33:45 +0900

    feat: 바뀐 dto 반영

--- apps/client/types/dtos/applyStudy.ts
+++ apps/client/types/dtos/applyStudy.ts
@@ -4,19 +4,28 @@ import type { DayOfWeekType, Time } from "types/entities/common/time";
 export interface StudyList {
   studyId: number;
   title: string;
-  studyType: StudyType;
-  notionLink?: string;
-  introduction?: string;
+  type: StudyType;
+  description: string;
+  descriptionNotionLink?: string;
+  mentorId: number;
   mentorName: string;
   dayOfWeek: DayOfWeekType;
   startTime: Time | null;
   endTime: Time | null;
   totalWeek: number;
   openingDate: string;
-  applicationEndDate: string;
+  applicationPeriod: {
+    startTime: string;
+    endTime: string;
+  };
+  totalRound: number;
+  semester: {
+    academicYear: number;
+    semesterType: "FIRST" | "SECOND";
+  };
 }
 
 export interface StudyListApiResponseDto {
-  appliedStudyId: number;
-  studyResponses: StudyList[];
+  appliedStudyIds: number[];
+  applicableStudies: StudyList[];
 }
