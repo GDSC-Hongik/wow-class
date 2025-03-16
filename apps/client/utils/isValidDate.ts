6c42e47958fe562fbfffb5871d0de4732799eff2
SeieunYoo <isho48239@gmail.com>
2025-03-17 04:26:24 +0900

    fix: kst 로 수정

--- apps/client/utils/isValidDate.ts
+++ apps/client/utils/isValidDate.ts
@@ -1,6 +1,6 @@
 export const getNowIsAfterStartDate = (startDate: string): boolean => {
   const now = new Date();
-  const start = new Date(`${startDate}`);
+  const start = new Date(`${startDate}+09:00`);
 
   console.log(now, now.getTime(), start, start.getTime(), "check2");
   return now.getTime() >= start.getTime();
@@ -8,7 +8,7 @@ export const getNowIsAfterStartDate = (startDate: string): boolean => {
 
 export const getNowIsBeforeEndDate = (endDate: string): boolean => {
   const now = new Date();
-  const end = new Date(endDate);
+  const end = new Date(`${endDate}+09:00`);
   console.log(now, end, "check3");
   return now.getTime() <= end.getTime();
 };
