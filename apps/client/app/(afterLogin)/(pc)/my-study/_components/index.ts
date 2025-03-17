2a3cc95afb0ea991a73e31790554504e9c6ad48e
SeieunYoo <isho48239@gmail.com>
2025-03-17 23:41:34 +0900

    fix: 나의 스터디 페이지 에러 터지는 부분 삭제

--- apps/client/app/(afterLogin)/(pc)/my-study/_components/index.ts
+++ apps/client/app/(afterLogin)/(pc)/my-study/_components/index.ts
@@ -4,6 +4,6 @@ export { default as DailyTaskCarousel } from "./DailyTaskCarousel";
 export { default as DailyTaskItem } from "./DailyTaskItem";
 export { default as DailyTasks } from "./DailyTasks";
 export { default as EmptyStudy } from "./EmptyStudy";
-export { default as Header } from "./Header";
+// export { default as Header } from "./Header";
 export { default as StudyAnnouncementList } from "./StudyAnnouncementList";
 export { default as StudyCurriculum } from "./StudyCurriculum";
