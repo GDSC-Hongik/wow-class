b4a91d6d1e7cc222b7b85d7194af45947d286349
Eugene Kim <67894159+eugene028@users.noreply.github.com>
2025-03-19 23:38:15 +0900

    [Feature] 어드민 스터디 상세 페이지 v2 버전 작업 (#200)
    * fix: 스터디 목록 불러오기 dto v2로 변경
    
    * fix: 스터디 기본 정보 조회 변경
    
    * feat: 스터디 공지 생성 v2 API로 변경
    
    * feat: navBar 목록 스터디 v2로 조회하도록 변경
    
    * fix: 기본 정보 header
    
    * feat: 스터디 상세 정보 header v2로 변경
    
    * feat: 출결번호 조회 기능
    
    * feat: 스터디 공지 생성
    
    * fix: 코드리뷰 반영
    
    * fix: 빌드에러 나는 부분 수정
    
    * fix: announcement 부분 수정
    
    * fix: 빌드에러 해결
    
    * fix: 코드리뷰 반영

--- apps/admin/app/studies/[studyId]/_components/announcement/CreateStudyAnnouncement.tsx
+++ apps/admin/app/studies/[studyId]/_components/announcement/CreateStudyAnnouncement.tsx
@@ -7,13 +7,14 @@ import { studyApi } from "apis/study/studyApi";
 import { tags } from "constants/tags";
 import useResizeTextarea from "hooks/useResizeTextarea";
 import { useRef, useState } from "react";
-import type { StudyAnnouncementType } from "types/entities/study";
+import type { CreateStudyAnnouncementType } from "types/entities/study";
 import { revalidateTagByName } from "utils/revalidateTagByName";
 import Button from "wowds-ui/Button";
 
 const CreateStudyAnnouncement = ({ studyId }: { studyId: string }) => {
   const [studyAnnouncement, setStudyAnnouncement] =
-    useState<StudyAnnouncementType>({
+    useState<CreateStudyAnnouncementType>({
+      studyId: parseInt(studyId, 10),
       title: "",
       link: "",
     });
@@ -23,13 +24,12 @@ const CreateStudyAnnouncement = ({ studyId }: { studyId: string }) => {
   useResizeTextarea([titleTextareaRef, linkTextareaRef], studyAnnouncement);
 
   const handlePublishAnnouncement = async (studyId: string) => {
-    const { success } = await studyApi.publishStudyAnnouncement(
-      parseInt(studyId, 10),
-      studyAnnouncement
-    );
+    const { success } =
+      await studyApi.publishStudyAnnouncement(studyAnnouncement);
     if (success) {
       revalidateTagByName(tags.announcements);
       setStudyAnnouncement({
+        studyId: parseInt(studyId, 10),
         title: "",
         link: "",
       });
