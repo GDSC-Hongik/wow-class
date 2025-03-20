c9741fc733e01a97d406743fb8e4c799b048cc2a
chocoboy08 <chocoboy0508@gmail.com>
2025-03-20 16:37:49 +0900

    feat: 커리큘럼, 공지 Tabs 추가

--- apps/client/app/(afterLogin)/(pc)/my-study/[studyId]/page.tsx
+++ apps/client/app/(afterLogin)/(pc)/my-study/[studyId]/page.tsx
@@ -1,7 +1,12 @@
 import { Space } from "@wow-class/ui";
 import Divider from "wowds-ui/Divider";
+import Tabs from "wowds-ui/Tabs";
+import TabsContent from "wowds-ui/TabsContent";
+import TabsItem from "wowds-ui/TabsItem";
+import TabsList from "wowds-ui/TabsList";
 
 import Header from "./_components/Header";
+import AnnouncementList from "./_components/StudyAnnouncement/AnnouncementList";
 import Curriculum from "./_components/StudyCurriculum/Curriclum";
 
 const StudyDetailPage = ({ params }: { params: { studyId: number } }) => {
@@ -10,8 +15,20 @@ const StudyDetailPage = ({ params }: { params: { studyId: number } }) => {
     <>
       <Header studyId={studyId} />
       <Space height={40} />
-      <Divider />
-      <Curriculum studyId={studyId} />
+      <Divider type="dark" />
+      <Tabs defaultValue="curriculum">
+        <TabsList>
+          <TabsItem value="curriculum">커리큘럼</TabsItem>
+          <TabsItem value="announcement">공지</TabsItem>
+        </TabsList>
+        <Divider style={{ marginTop: -3 }} type="dark" />
+        <TabsContent value="curriculum">
+          <Curriculum studyId={studyId} />
+        </TabsContent>
+        <TabsContent value="announcement">
+          <AnnouncementList studyId={studyId} />
+        </TabsContent>
+      </Tabs>
     </>
   );
 };
