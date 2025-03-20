a90275afe7464836ab5b240405d7bb1b97bf2925
Hwang Junheaok <strawji02@naver.com>
2025-03-21 00:38:19 +0900

    refactor: API V2로 변경

--- apps/client/types/dtos/myStudy.ts
+++ apps/client/types/dtos/myStudy.ts
@@ -34,7 +34,6 @@ export interface BasicStudyInfoDto {
   mentorId: number;
   mentorName: string;
 }
-
 interface StudyAnnouncementDto {
   studyAnnounceId: number;
   title: string;
@@ -42,15 +41,31 @@ interface StudyAnnouncementDto {
   createdDate: string;
 }
 
+interface StudyAnnouncementDtoV2 {
+  study: BasicStudyInfoDto;
+  studyAnnouncement: StudyAnnouncementDto;
+}
+
 export type StudyAnnouncementListDtoType = StudyAnnouncementDto[];
 
-export interface MyOngoingStudyInfoDto {
+export type StudyAnnouncementListDtoV2Type = StudyAnnouncementDtoV2[];
+
+interface OngoingStydyInfo {
   studyId: number;
+  studyName: string;
+  studyType: StudyType;
+  semester: {
+    academicYear: number;
+    semesterType: SemesterType;
+  };
+  mentorId: number;
+  mentorName: string;
 }
 
-export interface MyOngoingStudyInfoDtoV2 {
-  studyIds: number[];
-}
+export type MyOngoingStudyInfoDtoV2 = OngoingStydyInfo[];
+
+export interface StudyAllTodosDto {}
+
 interface StudyCurriculumDto {
   studyDetailId: number;
   period: {
