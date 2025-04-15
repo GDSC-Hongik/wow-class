6d815319d03731f25aeecd35fa5b6a7a6b928397
chocoboy08 <chocoboy0508@gmail.com>
2025-04-15 22:36:45 +0900

    feat: 변경된 dto에 맞춰 StudentListItem 수정

--- apps/admin/app/students/_components/StudentTable/StudentListItem.tsx
+++ apps/admin/app/students/_components/StudentTable/StudentListItem.tsx
@@ -6,35 +6,46 @@ import { formatNumberToPercent } from "utils/formatNumber";
 import Table from "wowds-ui/Table";
 import TextButton from "wowds-ui/TextButton";
 
+import BarGraph from "@/studies/[studyId]/_components/statics/graph/BarGraph";
+
 import { StudyTasksTds } from "./StudyTasks";
 
 const StudentListItem = ({
-  studyHistoryStatus,
-  isFirstRoundOutstandingStudent,
-  isSecondRoundOutstandingStudent,
-  name,
-  studentId,
-  discordUsername,
-  nickname,
-  githubLink,
+  member,
   studyTasks,
+  studyHistory,
+  achievements,
   assignmentRate,
   attendanceRate,
 }: StudyStudentApiResponseDto) => {
+  const { name, studentId, discordUsername, nickname } = member;
+  const { status, githubLink } = studyHistory;
   return (
     <>
       <Table.Td>
-        <StarCheckIcon checked={studyHistoryStatus === "COMPLETED"} />
+        <StarCheckIcon checked={status === "COMPLETED"} />
       </Table.Td>
       <Table.Td>
         <Text style={awardTextStyle} typo="body2">
-          <AwardIcon disabled={!isFirstRoundOutstandingStudent} />
+          <AwardIcon
+            disabled={
+              !achievements.some(
+                (item) => item.type === "FIRST_ROUND_OUTSTANDING_STUDENT"
+              )
+            }
+          />
           1차
         </Text>
       </Table.Td>
       <Table.Td>
         <Text style={awardTextStyle} typo="body2">
-          <AwardIcon disabled={!isSecondRoundOutstandingStudent} />
+          <AwardIcon
+            disabled={
+              !achievements.some(
+                (item) => item.type === "SECOND_ROUND_OUTSTANDING_STUDENT"
+              )
+            }
+          />
           2차
         </Text>
       </Table.Td>
@@ -51,8 +62,15 @@ const StudentListItem = ({
         />
       </Table.Td>
       <StudyTasksTds tasks={studyTasks} />
-      <Table.Td>{formatNumberToPercent(assignmentRate)}</Table.Td>
       <Table.Td>{formatNumberToPercent(attendanceRate)}</Table.Td>
+      <Table.Td>{formatNumberToPercent(assignmentRate)}</Table.Td>
+      <Table.Td>
+        <BarGraph
+          isToolTipActive={false}
+          percent={(attendanceRate + assignmentRate) / 2}
+          showPercent={false}
+        />
+      </Table.Td>
     </>
   );
 };
