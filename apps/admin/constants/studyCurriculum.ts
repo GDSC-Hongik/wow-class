d367698346165972237c5140b7841f7b13185fa8
SeieunYoo <101736358+SeieunYoo@users.noreply.github.com>
2025-04-19 17:46:31 +0900

    Update studyCurriculum.ts

--- apps/admin/constants/studyCurriculum.ts
+++ apps/admin/constants/studyCurriculum.ts
@@ -1,4 +1,4 @@
-export const studyCourseList = Array.from({ length: 12 }, (_, index) => {
+export const studyCourseList = Array.from({ length: 15 }, (_, index) => {
   const week = index + 1;
   return { text: `${week}회차`, value: `${week}` };
 });
